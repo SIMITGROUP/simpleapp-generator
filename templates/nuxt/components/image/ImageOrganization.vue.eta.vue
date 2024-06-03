72c257c9b9a2b6bf36c4c92fc19dc169fe7e39b0
kstan <kstan@simitgroup.com>
2024-06-03 18:31:43 +0800

    1.6.4b
    1. plenty of bug fix or ui enhancement of permission control, hide no permission menus, and disable action button on toolbar
    2. fix bugs on generate document numbering not read from session
    3. plenty of upload logo fix
    4. create tenant auto define some default value in branch and org
    5. disable bpmn server since not using it at this moment
    6. viewer after wont auto close for document with status control
    7. select/autocomplete block add new if no permission exists
    8. improve UI on tenant picker

--- templates/nuxt/components/image/ImageOrganization.vue.eta.vue
+++ templates/nuxt/components/image/ImageOrganization.vue.eta.vue
@@ -2,41 +2,59 @@
   <ImageToBase64Uploader
     v-if="changable"
     #default
-    class="w-20 h-20 place-content-center"
+    :class="`w-${sizenumber} h-${sizenumber} place-content-center bg-white`"
     @image-uploaded="handleBase64"
   >
-    <Image v-if="imageData!=''" :src="imageData"></Image>
-    <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="black" d="M21 10h-2V4h1V2H4v2h1v6H3a1 1 0 0 0-1 1v9h20v-9a1 1 0 0 0-1-1m-7 8v-4h-4v4H7V4h10v14z"/><path fill="black" d="M9 6h2v2H9zm4 0h2v2h-2zm-4 4h2v2H9zm4 0h2v2h-2z"/></svg>
+    <NuxtImg
+      class="w-full h-full"
+      :key="imageKey"
+      provider="myProvider"
+      :src="imagepath"
+    />
   </ImageToBase64Uploader>
-  <div v-else class="inline-block border rounded-lg w-20 h-20 bg-white">
-    <Image v-if="imageData!=''" :src="imageData"></Image>
-    <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="black" d="M21 10h-2V4h1V2H4v2h1v6H3a1 1 0 0 0-1 1v9h20v-9a1 1 0 0 0-1-1m-7 8v-4h-4v4H7V4h10v14z"/><path fill="black" d="M9 6h2v2H9zm4 0h2v2h-2zm-4 4h2v2H9zm4 0h2v2h-2z"/></svg>
+  <div
+    v-else
+    :class="`inline-block border rounded-lg w-${sizenumber} h-${sizenumber} bg-white`"
+  >
+    <NuxtImg class="w-full h-full" provider="myProvider" :src="imagepath" />
   </div>
 </template>
 <script lang="ts" setup>
 // import {KeyValue} from ''
 const props = defineProps<{
-  changable: boolean;
+  changable?: boolean;
+  orgRecordId?: string;
+  size: number;
 }>();
+const imageKey = ref(0);
+const orgRecordId = computed(
+  () => props.orgRecordId ?? getUserProfile()?.orgRecordId,
+);
+const xorgpath= getCurrentXorg() ? `${getCurrentXorg()}/` : 'MC0wLTA/'
+
+const imagepath = `${xorgpath}images/organization/${
+  orgRecordId.value
+}`;
+const sizenumber = props.size ?? 16;
 const imageData = ref("");
+watch(
+  () => props.orgRecordId,
+  () => imageKey.value++,
+);
 const handleBase64 = async (data: string) => {
   const keyvalue = {
-    key: "organizationLogo",
+    key: "org-" + props.orgRecordId,
     value: data,
   };
-
+  console.log("upload logo ", data);
   const uploadok = await useNuxtApp()
     .$OrganizationDoc()
     .getApi()
     .runUploadlogo(keyvalue);
   if (uploadok) {
-    await loadLogo();
+    const realpath = `/api/${imagepath}`;
+    await fetch(realpath, { cache: "reload", credentials: "include" });
+    imageKey.value++;
   }
 };
-const loadLogo = async () => {
-  await refreshOrgLogo();
-  imageData.value = getOrgLogo();
-};
-
-onMounted(async () => await loadLogo());
 </script>
