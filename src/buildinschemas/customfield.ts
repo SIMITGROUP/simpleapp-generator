08a711ef8a787bb956047650c80ed10a60214937
jhyong <jhyong@simitgroup.com>
2025-05-23 15:29:16 +0800

    Add Custom Field

---
+++ src/buildinschemas/customfield.ts
@@ -0,0 +1,35 @@
+import { IsolationType, SchemaType } from '../type';
+
+export const customfield: SchemaType = {
+  type: 'object',
+  'x-simpleapp-config': {
+    documentType: 'customfield',
+    documentName: 'customfield',
+    isolationType: IsolationType.org,
+    uniqueKey: 'collection',
+    documentTitle: 'collection'
+  },
+  properties: {
+    _id: { type: 'string' },
+    created: { type: 'string' },
+    updated: { type: 'string' },
+    createdBy: { type: 'string' },
+    updatedBy: { type: 'string' },
+    tenantId: { type: 'integer', default: 1 },
+    orgId: { type: 'integer', default: 1 },
+    branchId: { type: 'integer', default: 1 },
+    collection: {
+      type: 'string',
+      minLength: 2
+    },
+    form: {
+      type: 'object',
+      properties: {
+        schema: {
+          type: 'string',
+          minLength: 2
+        }
+      }
+    }
+  }
+};
